b2b48c4fb0161484f40888c17d81368b47ef87ee
Sharuf shaik <shaiksharuf95@gmail.com>
2022-11-27 23:40:49 +0530

    more queries added

---
+++ Insert-data.sql
@@ -0,0 +1,4 @@
+Insert into Students(Id,name,city)VALUES 
+(1,'Sharuf','Ongole'),
+(2,'Shaik','Guntur'),
+(3,'Zakeer','USA')
--- basic-table.sql
+++ basic-table.sql
@@ -3,3 +3,8 @@ Id Int,
 Name Varchar(100),
 City Varchar (20),
 );
+
+CREATE TABLE Cities(
+Id Int,
+Name Varchar(100),
+);
